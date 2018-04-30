a7ac8097e9e1f2625f269a9ccaf35d211c4c1268
RachelDoren <racheledoren@gmail.com>
2018-04-29 20:54:12 -0600

    Add active button filter functionality

--- script.js
+++ script.js
@@ -18,3 +18,14 @@ var span = document.getElementsByClassName("closes")[0];
 span.onclick = function() { 
   modal.style.display = "none";
 }
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
